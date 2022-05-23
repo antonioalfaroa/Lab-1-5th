ea6e65164d9e0db39c077cdd93def88cecc3b946
Antonio <78964462+antonioalfaroa@users.noreply.github.com>
2022-05-23 20:33:11 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main (void){
+  cout<<right<<setw(20)<<"Good Morning"<<endl;
+  cout<<right<<setw(20)<<"Sarah       "<<endl;
+  cout<<right<<setw(20)<<"Sunshine!   "<<endl;
+  cout<<endl<<endl;
+  cout<<right<<setw(10)<<"Good Morning"<<endl;
+  cout<<right<<setw(20)<<"Sarah"<<endl;
+  cout<<right<<setw(30)<<"Sunshine!"<<endl;
+  
+return 0;
+}
